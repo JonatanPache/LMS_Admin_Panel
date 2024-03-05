c9dea30133cfdc0e11af56f87ef3ee29d2a0ec52
JonatanPache <yonh.guitar777@gmail.com>
2024-03-05 09:23:41 -0400

    [4] -> 3. Create & Load Partials Layouts

--- project/routes/admin.js
+++ project/routes/admin.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/admin', function(req, res, next) {
-  res.render('dashboard');
+router.get("/admin", function(req, res, next) {
+  res.render("admin/dashboard");
 });
 
 module.exports = router;
