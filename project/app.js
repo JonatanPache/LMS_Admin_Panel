79432b6035767946df38dd2ccaca9636959aa6d7
JonatanPache <yonh.guitar777@gmail.com>
2024-03-06 21:11:14 -0400

    [4] -> 7. Layout - Add Category

--- project/app.js
+++ project/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
+var categoryRouter = require('./routes/category');
 
 var app = express();
 
@@ -27,6 +28,8 @@ app.use("/admin/:any",express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/', adminRouter);
+app.use('/', categoryRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
