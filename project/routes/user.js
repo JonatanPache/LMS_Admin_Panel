db5a52ab2ceb7bce4c529b99b1c88a755c700d22
JonatanPache <yonh.guitar777@gmail.com>
2024-03-07 11:03:51 -0400

    [4] -> 11. Layout - Add User

--- project/routes/user.js
+++ project/routes/user.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/admin", function(req, res, next) {
-  res.render("admin/dashboard");
+router.get("/admin/add-user", function(req, res, next) {
+  res.render("admin/add-user");
+});
+
+router.get("/admin/list-user", function(req, res, next) {
+  res.render("admin/list-user");
 });
 
 module.exports = router;
