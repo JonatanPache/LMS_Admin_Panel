d5ba0f140aad7e51e7c32a4ede3eaec02bea1562
JonatanPache <yonh.guitar777@gmail.com>
2024-03-07 12:21:49 -0400

    [4] -> 18. Layout - Days Settings

--- project/routes/settings.js
+++ project/routes/settings.js
@@ -6,4 +6,8 @@ router.get("/admin/currency-settings", function(req, res, next) {
   res.render("admin/currency-settings");
 });
 
+router.get("/admin/day-settings", function(req, res, next) {
+  res.render("admin/day-settings");
+});
+
 module.exports = router;
