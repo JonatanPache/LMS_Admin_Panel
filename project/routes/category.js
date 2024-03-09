ff4c1eab9ea9a470da5bf9cebb4ff057616b7d0e
JonatanPache <yonh.guitar777@gmail.com>
2024-03-08 22:32:45 -0400

    [5] -> 4. Check Category Existence

--- project/routes/category.js
+++ project/routes/category.js
@@ -1,31 +1,46 @@
 var express = require('express');
-
+var Sequelize = require('sequelize');
 var categoryModel = require("../models").Category;
 
 
 var router = express.Router();
-
+var Op = Sequelize.Op;
 
 router.route("/admin/add-category").get(function(req, res, next){
   
   res.render("admin/add-category");
 
 }).post( function(req, res, next){
-  
-  categoryModel.create({
-    name: req.body.name,
-    status: req.body.status,
-  }).then( (category) => {
 
-    if(category){
-      req.flash("success", "Category created successfully")
+  categoryModel.findOne({
+    where: {
+      name: {
+        [Op.eq]: req.body.name
+      }
+    }
+  }).then( (data) => {
+    if(data){
+      req.flash("error", "Category already created.")
       res.redirect("/admin/add-category");
     } else {
-      req.flash("error", "Failed to create category")
-      res.redirect("/admin/add-category");
+      categoryModel.create({
+        name: req.body.name,
+        status: req.body.status,
+      }).then( (category) => {
+    
+        if(category){
+          req.flash("success", "Category created successfully")
+          res.redirect("/admin/add-category");
+        } else {
+          req.flash("error", "Failed to create category")
+          res.redirect("/admin/add-category");
+        }
+    
+      }); 
     }
-
-  }); 
+  })
+  
+  
 
 
 });
