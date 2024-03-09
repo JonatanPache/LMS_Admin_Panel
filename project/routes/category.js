0806a763fb01d45b6f02fe33cd057536c1551008
JonatanPache <yonh.guitar777@gmail.com>
2024-03-08 22:05:51 -0400

    [5] -> 2. Create & Save Category

--- project/routes/category.js
+++ project/routes/category.js
@@ -1,10 +1,27 @@
 var express = require('express');
+
+var categoryModel = require("../models").Category;
+
+
 var router = express.Router();
 
-/* GET home page. */
-router.get("/admin/add-category", function(req, res, next) {
+
+router.route("/admin/add-category").get(function(req, res, next){
+  
   res.render("admin/add-category");
+
+}).post( function(req, res, next){
+  
+  categoryModel.create({
+    name: req.body.name,
+    status: req.body.status,
+  }).then( (category) => {
+    res.redirect("/admin/add-category");
+  }); 
+
+
 });
+
 
 router.get("/admin/list-category", function(req, res, next) {
   res.render("admin/list-category");
